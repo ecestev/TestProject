353d82e3162e4054f59d67400cdc0ca26f0ac0d5
Stephen Sampson <stephensampson@dal.ca>
2018-08-13 20:42:47 -0300

    Cleanup
    cleanup

--- CodeComposer/TestFramework/Tests.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-bool TestConfigIsInitReturnsFalseIfNotInitialized(void);
-bool TestInitializeConfigurationManagerReturnsConfigOk(void);
-bool TestConfigIsInitReturnsTrueIfInitialized(void);
