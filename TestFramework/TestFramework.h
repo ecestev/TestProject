8f8532dccbd288eeaa91e32e24809ff571fa50b9
Stephen Sampson <ssampson@metocean.com>
2018-08-13 13:26:16 -0300

    Modularized TestFramework.c
    Decluttered TestFramework.c by defining the structure containing the tests in a header file.

--- TestFramework/TestFramework.h
+++ TestFramework/TestFramework.h
@@ -1,3 +1,19 @@
 #pragma once
 
-#define TEST_DEFS = {Test1, Test2}
+/* Define a function pointer for a bool with no arguments */
+typedef bool(*BoolFuncPtr)(void);
+
+/* Define an object that will hold a pointer to the function to be called
+as well as a formatted string to be printed */
+typedef struct {
+	char string[64];
+	BoolFuncPtr fn;
+} TestFrame;
+
+#define TESTS {\
+{"TestConfigIsInitReturnsFalseIfNotInitialized", TestConfigIsInitReturnsFalseIfNotInitialized},\
+{ "TestInitializeConfigurationManagerReturnsConfigOk", TestInitializeConfigurationManagerReturnsConfigOk },\
+{ "TestConfigIsInitReturnsTrueIfInitialized", TestConfigIsInitReturnsTrueIfInitialized }\
+/* Above this line, add your tests as per the following format:*/\
+/* {FormattedTestStringToPrint, FunctionNameOfTestToCall} */\
+};
